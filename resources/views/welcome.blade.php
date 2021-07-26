4388e23c409538ea27755a8ceaeab8e55b9fa465
darday <darday1980@gmail.com>
2021-07-26 08:59:00 -0500

    Hola en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                   EDISON DAVID PACA CEPEDA . TEST 
+                                   EDISON DAVID PACA CEPEDA . TEST Hola 
                                 </div>
                             </div>
                         </div>
